b5b24b3e91b2747fdda074db1439af5a0e71f640
David Paz <davidmpaz@gmail.com>
2014-12-26 23:55:47 +0100

    Add test php project
    Sample project with modus ponens rules.
    
    Conflicts:
    	.gitignore

---
+++ src/Cid/Ruler/ModusPonens/Morgan1Trait.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Cid\Ruler\ModusPonens;
+
+trait Morgan1Trait
+{
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 */
+	public function printTestDeMorganDoubleNegation() {
+		// TODO implement this
+		throw new \Exception('Implement this action for rule: deMorganDoubleNegation.');
+	}
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 */
+	public function printTestDeMorganTautologyOr() {
+		// TODO implement this
+		throw new \Exception('Implement this action for rule: deMorganTautologyOr.');
+	}
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 */
+	public function printTestDeMorganTautologyAnd() {
+		// TODO implement this
+		throw new \Exception('Implement this action for rule: deMorganTautologyAnd.');
+	}
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 */
+	public function printTestDeMorganExcludeMiddle() {
+		// TODO implement this
+		throw new \Exception('Implement this action for rule: deMorganExcludeMiddle.');
+	}
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 */
+	public function printTestDeMorganNonContradiction() {
+		// TODO implement this
+		throw new \Exception('Implement this action for rule: deMorganNonContradiction.');
+	}
+}
