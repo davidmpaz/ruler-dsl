fcabb1689c9eb6b5da02004b57687cde2e5c6ba0
David Paz <davidmpaz@gmail.com>
2015-02-06 00:12:23 +0100

    Remove unessesary annotation

--- org.ruler/src/org/ruler/preferences/PropertiesProviderImpl.java
+++ org.ruler/src/org/ruler/preferences/PropertiesProviderImpl.java
@@ -4,7 +4,6 @@ import org.eclipse.core.resources.IProject;
 
 public class PropertiesProviderImpl implements PropertiesProvider {
 
-	@Override
 	public String getBasePkg(IProject project) {
 		return Properties.getBasePkg(project);
 	}
