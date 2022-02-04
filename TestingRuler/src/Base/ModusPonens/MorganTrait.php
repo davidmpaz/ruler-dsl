60436b35834fc105d5e36f389c288f8a27fb2900
David Paz <davidmpaz@gmail.com>
2022-02-04 11:31:30 +0100

    Added trait to test project

---
+++ TestingRuler/src/Base/ModusPonens/MorganTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Base\ModusPonens;
+
+trait MorganTrait
+{
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 * $this->context['q']
+	 */
+	public function printTestDeMorgan1() {
+		// TODO implement this
+		//throw new \Exception('Implement this action for rule: deMorgan1.');
+	}
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 * $this->context['q']
+	 */
+	public function printTestDeMorgan2() {
+		// TODO implement this
+		//throw new \Exception('Implement this action for rule: deMorgan2.');
+	}
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 * $this->context['q']
+	 */
+	public function printTestMorganCommutationOr() {
+		// TODO implement this
+		//throw new \Exception('Implement this action for rule: deMorganCommutationOr.');
+	}
+	/**
+	 * These parameters should be accessible from $this->context array
+	 *
+	 * $this->context['p']
+	 * $this->context['q']
+	 */
+	public function printTestMorganCommutationAnd() {
+		// TODO implement this
+		//throw new \Exception('Implement this action for rule: deMorganCommutationAnd.');
+	}
+}
